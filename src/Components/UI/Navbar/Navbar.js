22800339ebc5e8dbd020e7730901b56de9e80b34
EyalIlan <eyalilan8@gmail.com>
2021-06-23 23:55:19 +0300

    first structure for navbar

---
+++ src/Components/UI/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+    return (
+        <div className = "navbar">
+                <h2>Eyal Ilan</h2>
+            
+                <div className="navbar-list">
+                    <li><a href="">test </a></li>
+                    <li><a href="">test </a></li>
+                    <li><a href="">test </a></li>
+                    <li><a href="">test </a></li>
+                </div>
+
+
+        </div>
+    )
+}
