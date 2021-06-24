84d6ff202527b0fa6158b430dd1f4a944adaf6c7
EyalIlan <eyalilan8@gmail.com>
2021-06-25 00:43:37 +0300

    first build responsive navbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
         <BrowserRouter>
-          <div>
+          <div className="main">
             <Navbar></Navbar>
             <Route path='/' component={HomePage}></Route>
           </div>
--- src/Components/UI/Navbar/Navbar.js
+++ src/Components/UI/Navbar/Navbar.js
@@ -4,13 +4,18 @@ import './Navbar.css'
 export default function Navbar() {
     return (
         <header className = "navbar">
-                <h2 className="text-big">Eyal Ilan</h2>
+                <h2 className="text-big">E<span id="logo-higelight">Y</span>AL ILAN</h2>
             
                 <div className="navbar-list">
-                    <a className="text-medium" href="">test </a>
-                    <a  className="text-medium"  href="">test </a>
-                    <a className="text-medium"  href="">test </a>
-                    <a  className="text-medium" href="">test </a>
+                    
+
+                    <a className="text-medium" href="">Home</a>
+                    
+                    <a  className="text-medium"  href="">About</a>
+                    
+                    <a className="text-medium"  href="">Projects </a>
+                    
+                    <a  className="text-medium" href="">Contact</a>
                 </div>
 
 
