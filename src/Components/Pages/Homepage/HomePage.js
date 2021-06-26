0f57475cb34da2665018d0dfd68c9828c0a4ab39
EyalIlan <eyalilan8@gmail.com>
2021-06-26 19:54:43 +0300

    add main page headline

--- src/Components/Pages/Homepage/HomePage.js
+++ src/Components/Pages/Homepage/HomePage.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import './HomePage.css'
 
 export default function HomePage() {
     return (
-        <div>
-        
+       <div>
+        <div className="homepage">
+            
+            <div className="headline">
+            <h1>Eyal Ilan</h1>
+            <h2>Full Stack Web Developer</h2>
+
+            </div>
         </div>
+     </div>
     )
 }
