2fbc5a41a974e4266d201861d20e0924e64467ec
EyalIlan <eyalilan8@gmail.com>
2021-06-27 23:28:22 +0300

    skill boxes structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import React, { useState } from 'react'
+
 import {Route,BrowserRouter} from 'react-router-dom'
 import Navbar from './Components/UI/Navbar/Navbar'
 import HomePage from './Components/Pages/Homepage/HomePage'
+
+
+
 function App() {
   return (
     <div className="App">
--- src/Components/Pages/Homepage/HomePage.js
+++ src/Components/Pages/Homepage/HomePage.js
@@ -10,8 +10,25 @@ export default function HomePage() {
             <h1>Eyal Ilan</h1>
             <h2>Full Stack Web Developer <i class="fas fa-laptop-code"></i></h2>
 
+        </div>
+        </div>
+
+      <section className="skills-boxes">
+        <div className="container">
+        <div className="grid">
+            <div className="box">
+        
+            </div>
+            <div className="box">
+
+            </div>
+            <div className="box">
+
             </div>
         </div>
+        </div> 
+      </section>
      </div>
+     
     )
 }
