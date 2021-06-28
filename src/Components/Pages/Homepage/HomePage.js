9dc0af417005ebb8752bdaabcfccf8751ec26342
EyalIlan <eyalilan8@gmail.com>
2021-06-28 23:23:09 +0300

    responsive skill boxes section

--- src/Components/Pages/Homepage/HomePage.js
+++ src/Components/Pages/Homepage/HomePage.js
@@ -18,6 +18,7 @@ export default function HomePage() {
           <h2 className="text-medium">I Specialize In</h2>
           <h4 className="text-small">Creating Fast Beatiful Web Applications</h4>
           <div className="grid">
+
             <div className="box">
               <img src="/images/programmer.png" alt="" />
               <h3 className="text-medium">passionate</h3>
@@ -25,9 +26,13 @@ export default function HomePage() {
             </div>
             <div className="box">
               <img src="/images/responsive.png" alt="" />
+              <h3 className="text-medium">full Stack Developer</h3>
+              <p>i like to code and mange diffrent parts of the projects i very versital and flexible and my way of thinking and i enjoy bringing ides to life</p>
             </div>
             <div className="box">
               <img src="/images/workout.png" alt="" />
+              <h3 className="text-medium">Leadership</h3>
+              <p>I enjoy the challenge of any project thinking my way to solve any problem. i like the reseponsibility and Leadership. I genuinely care about other people, and love helping fellow programers</p>
             </div>
           </div>
         </div>
